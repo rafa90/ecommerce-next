2b8e7a0ada4896d0ab6f1b31853d8cd472c97160
Rafael90 <rafa.ricardo.mendoza@gmail.com>
2025-07-21 13:16:32 -0300

    corrección add server url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,10 @@
 export const ENV = {
     //SERVER_HOST: "http://localhost:1337",
-    SERVER_HOST:"https://ecommerce-strapi-production-c44e.up.railway.app",
+    //SERVER_HOST:"https://ecommerce-strapi-production-c44e.up.railway.app",
+    SERVER_HOST:"https://ecommerce-strapiv2.up.railway.app",
     //API_URL: "http://localhost:1337/api",
-    API_URL:"https://ecommerce-strapi-production-c44e.up.railway.app/api",
+   // API_URL:"https://ecommerce-strapi-production-c44e.up.railway.app/api",
+     API_URL:"https://ecommerce-strapiv2.up.railway.app",
     ENDPOINTS: {
         AUTH:{
            REGISTER: "auth/local/register",
